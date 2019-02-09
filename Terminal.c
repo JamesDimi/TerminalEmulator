d06358856bf5e3beeeb27e03f5cc2d2604da0793
jamesdimi <ddimitrakop0@gmail.com>
2019-02-09 22:29:41 +0200

    tested the branching and commiting on linux

---
+++ Terminal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// This will be the first excersize
+int auebsh1()
+{
+    printf("Hello from auebsh1!\n");
+    return 0;
+}
+
+// This will be the second excersize
+int auebsh2()
+{
+    printf("Hello from auebsh2!\n");
+    return 0;
+}
+
+
+// Runs the certain Terminal
+int main()
+{
+    auebsh1();
+    auebsh2();
+
+    printf("Terminal Finished...\nClosing...\n");
+
+    return 0;
+}
