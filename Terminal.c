531478f2aa0dee001a92972a176b5bc78d64ba1b
jamesdimi <ddimitrakop0@gmail.com>
2019-02-09 23:18:12 +0200

    added old dimi solution at exercise1

--- Terminal.c
+++ Terminal.c
@@ -1,28 +1,54 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
 
 // This will be the first excersize
-int auebsh1()
+int auebsh1(void)
 {
-    printf("Hello from auebsh1!\n");
-    return 0;
-}
+	char str[255];
+	char *cmd;
+	int status;
+	pid_t pid, waitPid;
+	printf("\n--Welcome to auebsh1--\n");
+	printf("auebsh1> ");
 
-// This will be the second excersize
-int auebsh2()
-{
-    printf("Hello from auebsh2!\n");
-    return 0;
-}
+	while (fgets(str, 255, stdin)) {
+		pid = fork();
 
+		if (pid < 0) {
+
+			perror("ERROR: Child couldnt be created!\n");
+			return -1;
+		} 
+        if (pid == 0) {
+			// When using fgets it also reads the newLine character.
+			// We must remove it using strtok
+			cmd = strtok(str, "\n");
+			execlp(cmd, cmd, NULL);
+			perror("There is no such instruction!!\n");
+			return -1;
+		}
+        else {
+			waitPid = wait(&status);
+
+			if (waitPid == -1) {
+				perror("Failed to wait for child to be executed! \n");
+			}
+			printf("\nauebsh1> ");
+		}
+	}
+
+	printf("\nAuebsh1 Terminated!\n");
+	return 1;
+}
 
 // Runs the certain Terminal
 int main()
 {
     auebsh1();
-    auebsh2();
 
     printf("Terminal Finished...\nClosing...\n");
-
     return 0;
 }
